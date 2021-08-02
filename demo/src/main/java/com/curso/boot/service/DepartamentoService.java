62c9513f63372dbf931bed766842c579e56dcb2b
costelinha <andregomesngr@gmail.com>
2021-08-02 15:23:08 -0300

    add action create in Departamento

--- demo/src/main/java/com/curso/boot/service/DepartamentoService.java
+++ demo/src/main/java/com/curso/boot/service/DepartamentoService.java
@@ -4,11 +4,19 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.curso.boot.repository.DepartamentoRepository;
 import com.curso.boot.model.Departamento;
 
 @Service
+@Transactional
 public class DepartamentoService {
 	@Autowired
 	private DepartamentoRepository departamentoRepository;
+
+	public void save(Departamento departamento) {
+		// TODO Auto-generated method stub
+		departamentoRepository.save(departamento);
+	}
 }
--- demo/src/main/java/com/curso/boot/web/controller/DepartamentoController.java
+++ demo/src/main/java/com/curso/boot/web/controller/DepartamentoController.java
@@ -1,11 +1,18 @@
 package com.curso.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.curso.boot.model.Departamento;
+import com.curso.boot.service.DepartamentoService;
 
 @Controller
 @RequestMapping(path="/departamentos")
 public class DepartamentoController {
+	@Autowired
+	private DepartamentoService departamentoService;	
 
 	@RequestMapping(path="/listar")
 	public String list() {
@@ -13,7 +20,13 @@ public class DepartamentoController {
 	}
 	
 	@RequestMapping(path="/cadastrar") 
-	public String register() {
+	public String register(Departamento departamento) {
 		return "/departamento/cadastro";
+	}
+	
+	@RequestMapping(path="/salvar", method=RequestMethod.POST)
+	public String save(Departamento departamento) {
+		departamentoService.save(departamento);
+		return "redirect:/departamentos/cadastro";
 	}
 }
